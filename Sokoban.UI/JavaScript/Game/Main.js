3df5f7143997d750bb88328c0bf103c1bcf1316f
Walid Sultan <walidalysultan@gmail.com>
2015-04-08 19:03:35 +0200

    Added method for parsing xml files

---
+++ Sokoban.UI/JavaScript/Game/Main.js
@@ -0,0 +1,11 @@
+﻿var level1 = '../Levels/MicroCosmos/level0.xml';
+var levelDoc = loadXMLDoc(level1);
+var levelRows = levelDoc.getElementsByTagName("L");
+
+for (i = 0; i < levelRows.length; i++) {
+    var row = levelRows[i].childNodes[0].nodeValue;
+    for (var j = 0; j < row.length; j++) {
+        document.write(row[j]);
+    }
+    document.write("<br>");
+}
