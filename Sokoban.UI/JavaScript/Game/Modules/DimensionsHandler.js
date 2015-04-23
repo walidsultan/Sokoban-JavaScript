dc85fccbe925c7134939e4b92c3e76d08fd9ea01
Walid Sultan <walidalysultan@gmail.com>
2015-04-23 02:06:58 +0200

    Added SFF dimensions handling

--- Sokoban.UI/JavaScript/Game/Modules/DimensionsHandler.js
+++ Sokoban.UI/JavaScript/Game/Modules/DimensionsHandler.js
@@ -1,9 +1,8 @@
 ﻿(function (ns) {
     ns.DimensionsHandler = skui.extend(function () {
         $(window).resize(this.setBlocksDimensions.bind(this));
-        this.backgroundRatio = 1280 / 1024;
-        this.contentHeightRatio = 7 / 12.6;
-        this.contentWidthRatio = 15 / 19;
+        this.SFFWidth = 900;
+        this.SFFHeight = 600;
         $(window).on('setLevelDimensions', this.setLevelDimensions.bind(this))
     }, {
         setBlocksDimensions: function () {
@@ -11,6 +10,17 @@
             var windowWidth = $(window).width();
             var windowHeight = $(window).height();
             var windowSize = 0;
+
+            if (windowWidth <= this.SFFWidth || windowHeight <= this.SFFHeight) {
+                this.contentHeightRatio = .9;
+                this.contentWidthRatio = .9;
+                this.backgroundRatio = this.levelWidth / this.levelHeight;
+            } else {
+                this.contentHeightRatio = 7 / 12.6;
+                this.contentWidthRatio = 15 / 19;
+                this.backgroundRatio = 1280 / 1024;
+            }
+
             if (windowWidth >= windowHeight * this.backgroundRatio) {
                 windowSize = windowHeight * this.backgroundRatio;
             } else {
@@ -21,8 +31,8 @@
             //Top and left margin adjustment
             var leftMargin = 0;
             var topMargin = 0;
-            var zoomFactorWidth=windowSize * this.contentHeightRatio / this.levelHeight;
-            var zoomFactorHeight = windowSize * this.contentWidthRatio / this.levelWidth;
+            var zoomFactorWidth=windowSize *this.contentHeightRatio / this.levelHeight;
+            var zoomFactorHeight = windowSize *this.contentWidthRatio / this.levelWidth;
             var zoomFactor = Math.min(zoomFactorWidth,zoomFactorHeight);
             
             if (windowWidth >= windowHeight * this.backgroundRatio) {
