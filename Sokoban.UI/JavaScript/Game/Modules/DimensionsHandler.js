2e06a479ac4d837a8fd78236a765abc985b566ed
walidsultan <walidalysultan@gmail.com>
2015-04-24 20:28:45 +0200

    Fixed dimensions bug in mobiles for certain levels

--- Sokoban.UI/JavaScript/Game/Modules/DimensionsHandler.js
+++ Sokoban.UI/JavaScript/Game/Modules/DimensionsHandler.js
@@ -2,7 +2,7 @@
     ns.DimensionsHandler = skui.extend(function () {
         $(window).resize(this.setBlocksDimensions.bind(this));
         this.SFFWidth = 900;
-        this.SFFHeight = 600;
+        this.SFFHeight = 680;
         $(window).on('setLevelDimensions', this.setLevelDimensions.bind(this));
     }, {
         setBlocksDimensions: function () {
@@ -10,9 +10,9 @@
             var windowWidth = $(window).width();
             var windowHeight = $(window).height();
             var windowSize = 0;
-
-            if (windowWidth <= this.SFFWidth || windowHeight <= this.SFFHeight) {
-                this.contentHeightRatio = .9;
+            var isSFF=windowWidth <= this.SFFWidth || windowHeight <= this.SFFHeight;
+            if (isSFF) {
+                this.contentHeightRatio = .95;
                 this.contentWidthRatio = .9;
                 this.backgroundRatio = this.levelWidth / this.levelHeight;
             } else {
@@ -24,7 +24,7 @@
             if (windowWidth >= windowHeight * this.backgroundRatio) {
                 windowSize = windowHeight * this.backgroundRatio;
             } else {
-                windowSize = windowWidth ;
+                windowSize = windowWidth ;  
             }
             $('body').css({ 'background-size': windowSize });
 
@@ -32,15 +32,27 @@
             var leftMargin = 0;
             var topMargin = 0;
             var zoomFactorWidth=windowSize *this.contentHeightRatio / this.levelHeight;
-            var zoomFactorHeight = windowSize *this.contentWidthRatio / this.levelWidth;
-            var zoomFactor = Math.min(zoomFactorWidth,zoomFactorHeight);
-            
+            var zoomFactorHeight = windowSize * this.contentWidthRatio / this.levelWidth;
+
+            var zoomFactorMax = Math.max(zoomFactorWidth, zoomFactorHeight);
+            var zoomFactor = 0;
+
+            if (zoomFactorMax * this.levelWidth <= windowWidth * this.contentWidthRatio && zoomFactorMax * this.levelHeight <= windowHeight * this.contentHeightRatio) {
+                zoomFactor = zoomFactorMax;
+            } else {
+                zoomFactor = Math.min(zoomFactorWidth, zoomFactorHeight);
+            }
+
             if (windowWidth >= windowHeight * this.backgroundRatio) {
                 leftMargin = (windowWidth - this.levelWidth * zoomFactor) / 2
                 topMargin = (windowHeight - this.levelHeight * zoomFactor) / 2;
             } else {
                 leftMargin = (windowWidth - this.levelWidth * zoomFactor) / 2;
-                topMargin = (windowWidth / this.backgroundRatio - this.levelHeight * zoomFactor) / 2;
+                if (isSFF) {
+                    topMargin = (windowHeight - this.levelHeight * zoomFactor) / 2;
+                } else {
+                    topMargin = (windowWidth / this.backgroundRatio - this.levelHeight * zoomFactor) / 2;
+                }
             }
             $('body .gameContainer').css({ 'margin-left': leftMargin, 'margin-top': topMargin });
 
