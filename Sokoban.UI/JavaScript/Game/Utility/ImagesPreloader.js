ba82e366860d5ec9e96373df543a169c07200fce
walidsultan <walidalysultan@gmail.com>
2015-04-24 21:49:21 +0200

    Added images preloader

---
+++ Sokoban.UI/JavaScript/Game/Utility/ImagesPreloader.js
@@ -0,0 +1,16 @@
+﻿function preload() {
+    var images = new Array()
+    for (i = 0; i < preload.arguments.length; i++) {
+        images[i] = new Image()
+        images[i].src = preload.arguments[i]
+    }
+}
+preload(
+    "../Images/Wall.png",
+    "../Images/Box.png",
+    "../Images/BoxOnTarget.png",
+    "../Images/greenBox.png",
+    "../Images/Floor.png",
+    "../Images/Target.png",
+    "../Images/Ghost.png"
+)
