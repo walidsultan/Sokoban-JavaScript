c2c7c93c2dc76470ee60848294a9b4b874003f58
Walid Sultan <walidalysultan@gmail.com>
2015-05-06 17:57:31 +0200

    Added timer to SFF view

--- Sokoban.UI/JavaScript/Game/Views/Game.js
+++ Sokoban.UI/JavaScript/Game/Views/Game.js
@@ -51,6 +51,7 @@
         },
         setTimer: function () {
             $('body .gameStatusContainer .time').text(this.gameTime);
+            $('body .sffNavigator .timer').text(this.toHHMMSS(this.gameTime));
 
             this.gameTime++;
         },
@@ -119,6 +120,18 @@
             setTimeout(function () {
                 $('.gameContainer .errorBlock').remove();
             }, 500);
+        },
+        toHHMMSS : function (timeInt) {
+            var sec_num = parseInt(timeInt, 10);
+            var hours   = Math.floor(sec_num / 3600);
+            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+            if (hours   < 10) {hours   = "0"+hours;}
+            if (minutes < 10) {minutes = "0"+minutes;}
+            if (seconds < 10) {seconds = "0"+seconds;}
+            var time    = hours+':'+minutes+':'+seconds;
+            return time;
         }
     });
 })(skui.resolve('app.ui'));
