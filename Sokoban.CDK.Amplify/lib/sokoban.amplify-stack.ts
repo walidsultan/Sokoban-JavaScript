a4ad7ed4794abffe5473f3be459635f700bda398
Walid Sultan <walidalysultan@gmail.com>
2024-06-08 01:34:19 -0700

    Moved amplify.yml to the root of the branch

--- Sokoban.CDK.Amplify/lib/sokoban.amplify-stack.ts
+++ Sokoban.CDK.Amplify/lib/sokoban.amplify-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as amplify from "@aws-cdk/aws-amplify";
+import { CfnApp, CfnBranch } from 'aws-cdk-lib/aws-amplify';
 
 export class SokobanAmplifyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,12 +8,15 @@ export class SokobanAmplifyStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    const amplifyApp = new amplify.App(this, "Sokoban-JS", {
-      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
-        owner: "walidsultan",
-        repository: "Sokoban-JavaScript",
-        oauthToken: cdk.SecretValue.secretsManager('github-token'),
-      }),
+    const amplifyApp = new CfnApp(this, 'Sokoban-JS', {
+      name: 'Sokoban-JS-Amplify',
+      repository: 'https://github.com/walidsultan/Sokoban-JavaScript',
+      oauthToken: cdk.SecretValue.secretsManager('github-token').unsafeUnwrap()
+    });
+
+    new CfnBranch(this, 'MasterBranch', {
+      appId: amplifyApp.attrAppId,
+      branchName: 'master' // you can put any branch here (careful, it will listen to changes on this branch)
     });
   }
 }
